11a3fd1ea71b1bdc27d1e37a27d27f435092ce79
khizr99 <haidar.md21@iiits.in>
2023-08-20 13:17:25 +0530

    Mountain

--- DSA-PN/3_Mountain.cpp
+++ DSA-PN/3_Mountain.cpp
@@ -1,34 +1,45 @@
 #include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long LL;
-typedef pair<int, int> pii;
-typedef pair<LL, LL> pll;
-typedef pair<string, string> pss;
 typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef vector<pii> vii;
-typedef vector<LL> vl;
-typedef vector<vl> vvl;
-typedef unordered_set<int> us;
-#define fr(a, b, c) for (int(a) = (b); (a) < (c); ++(a)) //fr = for
-#define frn(a, b, c) for (int(a) = (b); (a) <= (c); ++(a))
-#define frd(a, b, c) for (int(a) = (b); (a) >= (c); --(a))
-#define frsq(a, b, c) for (int(a) = (b); (a) * (a) <= (c); ++(a))
-#define frc(a, b, c) for (char(a) = (b); (a) <= (c); ++(a))
-#define freach(a, b) for (auto&(a) : (b))
-#define rep(i, n) fr(i, 0, n)  //rep is simple for loop
-#define repn(i, n) frn(i, 1, n)
-#define sqr(x) ((LL)(x) * (x)) //sqr  = square
-#define fi first
-#define se second
-#define mp make_pair
 #define pb push_back
-#define all(v) v.begin(), v.end()
-#define alla(arr, sz) arr, arr + sz
-#define sz(v) (int)v.size() //sz = size
-#define sort(v) sort(all(v))
-#define rev(v) reverse(all(v)) // rev = reverse
-#define sorta(arr, sz) sort(alla(arr, sz))
-#define reva(arr, sz) reverse(alla(arr, sz))
-#define tc(t) while (t--)
+
+int highestMountain(vi arr){
+    int n = arr.size(), largest = 0;
+
+    for(int i=1; i<=n-2;){
+        if(arr[i]>arr[i-1]&&arr[i]>arr[i+1]){
+            //Then
+            int cnt = 1;
+            int j=i;
+            //counting backwards(left)
+            while(j>=1&&arr[j]>arr[j-1]){
+                j--;
+                cnt++;
+            }
+            //counting forward(right)
+            while(i<=n-2&&arr[i]>arr[i+1]){
+                i++;
+                cnt++;
+            }
+            largest = max(largest,cnt);
+        }
+        else{
+            i++;
+        }
+    }
+    return largest;
+}
+int main(){
+    vi arr;
+    int n;
+    cout<<"......................"<<endl;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        int temp;
+        cin>>temp;
+        arr.pb(temp);
+    }
+        cout<<highestMountain(arr)<<endl;
+ 
+}

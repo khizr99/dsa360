07c09b3d5b981f77f275f67dd6c16da8f8981be1
khizr99 <haidar.md21@iiits.in>
2023-08-26 17:35:53 +0530

    Largest_Rectangle_In_Histogram

--- DSA-PN/14_largestRect.cpp
+++ DSA-PN/14_largestRect.cpp
@@ -3,16 +3,28 @@
 using namespace std;
 typedef vector<int> vi;
 typedef pair<int, int> pii;
+typedef stack<int> st;
 #define pb push_back
 #define all(v) v.begin(), v.end()
 #define srt(v) sort(all(v)) //Sorting of vector
 typedef unordered_set<int> us;
 
-int largestRectangleArea(vector<int>& heights) {
-       
+int largestRectangleArea(vi& heights) {
+        st st;
+        int maxA = 0;
         int n = heights.size();
-        vi l(n,0), r(n,0);
-        
+        for(int i=0; i<=n; i++){
+            while((!st.empty()) && (i==n or heights[st.top()]>=heights[i])){
+                int height = heights[st.top()];
+                st.pop();
+                int width;
+                if(st.empty()) width = i;
+                else width = i-st.top()-1;
+        maxA = max(maxA, width*height);
+            }
+            st.push(i);
+        }
+        return maxA;
     }
 
 int main(){

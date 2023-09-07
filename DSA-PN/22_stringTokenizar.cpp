e8dbde5c0ab6a228728035e4933937cb30192450
khizr99 <haidar.md21@iiits.in>
2023-09-07 20:54:39 +0530

    StringTokenization

---
+++ DSA-PN/22_stringTokenizar.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+#define pb push_back
+typedef unordered_set<int> us;
+
+char *mystrtok(char *str, char delim){
+    static char *input = NULL;
+    if(str!=NULL){
+        input=str;
+    }
+    if(input==NULL){
+        return NULL;
+    }
+    char *token = new char[strlen(input)+1];
+    int i=0;
+    for(;str[i]!='\0'; i++){
+        if(input[i]!=delim) token[i]=input[i];
+        else {
+            token[i] = '\0';
+            input = input+i+1;
+            return token;
+        }
+    }
+    //out of the loop
+    token[i] = '\0';
+    //reset the input as well
+    input = NULL;
+    return token;
+    
+}
+int main(){
+    char s[1000];
+    cin.getline(s,1000);
+
+    //strtok()
+    char *token = mystrtok(s, ' ');
+
+    while(token!=NULL){
+        cout<<token<<endl;
+        token = mystrtok(NULL,' ');
+    }
+
+    return 0;
+}
